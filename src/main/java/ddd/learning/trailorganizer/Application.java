4a5582182a513b55bdc374f1ec3ab5c6a8d89c99
Julia Burgard <burgard@synyx.de>
2019-07-28 18:04:44 +0200

    Add an simple application class to show all trails
    * move the hardcoded trail library to the production code,
    because in a first step we save the trials in memory

---
+++ src/main/java/ddd/learning/trailorganizer/Application.java
@@ -0,0 +1,24 @@
+package ddd.learning.trailorganizer;
+
+import ddd.learning.trailorganizer.api.ConsoleAdapter;
+import ddd.learning.trailorganizer.application.TrailReader;
+import ddd.learning.trailorganizer.domain.Trail;
+import ddd.learning.trailorganizer.infrastructure.HardcodedTrailLibrary;
+
+import static java.util.Collections.singletonList;
+
+
+/**
+ * @author  Julia Burgard - burgard@synyx.de
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        TrailReader trailReader = new TrailReader(new HardcodedTrailLibrary(
+                    singletonList(new Trail("Trail des Vosges"))));
+        ConsoleAdapter consoleAdapter = new ConsoleAdapter(trailReader);
+
+        consoleAdapter.showAllTrails();
+    }
+}
