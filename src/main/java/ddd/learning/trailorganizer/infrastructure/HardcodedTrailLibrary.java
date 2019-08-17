2107f4bb2bf91ac86d1c79e0bf2dedf2f1020b8c
Julia Burgard <burgard@synyx.de>
2019-08-17 16:41:04 +0200

    Hardcode some example data to the in memory repo

--- src/main/java/ddd/learning/trailorganizer/infrastructure/HardcodedTrailLibrary.java
+++ src/main/java/ddd/learning/trailorganizer/infrastructure/HardcodedTrailLibrary.java
@@ -5,6 +5,7 @@ import ddd.learning.trailorganizer.domain.Trail;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -21,6 +22,6 @@ public class HardcodedTrailLibrary implements IObtainTrails {
     @Override
     public List<Trail> findAllTrails() {
 
-        return allTrails;
+        return Arrays.asList(new Trail("Trail des vosges", 10.0), new Trail("Baden halb-marathon", 21.0));
     }
 }
